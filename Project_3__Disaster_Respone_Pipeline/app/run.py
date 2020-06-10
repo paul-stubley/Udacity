3a672b997cf5af25ff864658c7f167adfcf70fa9
paul-stubley <66024622+paul-stubley@users.noreply.github.com>
2020-06-10 11:31:01 +0100

    Preparing to host on Heroku
    Created:
    - Procfile
    - requirements.txt

--- Project_3__Disaster_Respone_Pipeline/app/run.py
+++ Project_3__Disaster_Respone_Pipeline/app/run.py
@@ -7,7 +7,7 @@ from nltk.tokenize import word_tokenize
 
 from flask import Flask
 from flask import render_template, request, jsonify
-from plotly.graph_objs import Bar
+from plotly.graph_objs import Bar, Histogram
 import joblib
 from sqlalchemy import create_engine
 
@@ -15,6 +15,12 @@ from sqlalchemy import create_engine
 app = Flask(__name__)
 
 def tokenize(text):
+    '''Tokeniser function to be used when the model is called
+    Args:
+        text (str) - string to be tokenised
+    Returns:
+        (python list) - list of tokens
+    '''
     tokens = word_tokenize(text)
     lemmatizer = WordNetLemmatizer()
 
@@ -25,26 +31,49 @@ def tokenize(text):
 
     return clean_tokens
 
-# load data
-engine = create_engine('sqlite:///../data/DisasterResponse.db')
-df = pd.read_sql_table('disaster_response', engine)
-
-# load model
-model = joblib.load("../models/classifier.pkl")
-
-
-# index webpage displays visuals and receives user input text for model
-@app.route('/')
-@app.route('/index')
-def index():
-    
+def get_chart_object(df):
+    '''This function prepares the three charts for use on the homepage
+    Args:
+        df (pd.DataFrame) - the dataframe to plot
+    Returns:
+        graphs (list of dicts) - the graphs list to be passed to plotly frontend
+    '''
     # extract data needed for visuals
     genre_counts = df.groupby('genre').count()['message'].sort_values(ascending=False)
     genre_names = list(genre_counts.index.str.title())
 
     category_counts = df.sum(axis=0, numeric_only = True).sort_values()
     category_names = list(category_counts.index.str.replace('_', ' ').str.title())   
+    
+    # Dist plot
+    df_hist = df[['genre','message']]
+    df_hist['message_length'] = df_hist.message.str.split(' ').apply(len)
+    colors = (['rgb(69,48,140)','rgb(139,59,156)','rgb(234,217,169)'])
+    plot_order = df_hist.groupby('genre')['message'].count().sort_values(ascending=False).index
+    x = [df_hist.loc[df_hist.genre==g,'message_length'].apply(lambda x: min(x,100)) for g in plot_order]
+    
+    hist_data = [[Histogram(
+                    x=dat,
+                    name= plot_order.str.title()[i],
+                    opacity = j[1],
+                    marker = {'color':colors[i]},
+                    xbins=dict(size=1),
+                    histnorm= j[0]
+                ) for i,dat in enumerate(x)] for j in ((None,0.9), ('percent',0.6))]
 
+    hist_layout =  [{
+                'barmode':'overlay',
+                'title': title,
+                'xaxis': {
+                    'range': [0,100],  
+                    'title': "Message length (words)"
+                },
+                'yaxis': {
+                    'visible': False,  
+                },
+                'legend' : {'x':0.85,'y':0.9}
+            } for title in ['Distribution of Message Lengths','Distribution of Message Lengths (Normalised)']]
+    
     # create visuals
     graphs = [
         {
@@ -57,11 +86,8 @@ def index():
                     textposition='outside',
                     cliponaxis = False,
                     marker={'color': genre_counts
-                        , 'colorscale': 'Agsunset_r'}
-                    
+                        , 'colorscale': 'Agsunset_r'}   
                 )
- 
-
             ],
 
             'layout': {
@@ -74,6 +100,17 @@ def index():
                     #'title': "Genre"
                 }
             }
+        },
+        {
+            'data': hist_data[0],
+
+            'layout': hist_layout[0],
+        },
+
+        {
+            'data': hist_data[1],
+
+            'layout': hist_layout[1],
         },
         {
             'data': [
@@ -105,7 +142,22 @@ def index():
             }
         }
     ]
-    
+    return graphs
+
+
+
+# load data
+engine = create_engine('sqlite:///../data/DisasterResponse.db')
+df = pd.read_sql_table('disaster_response', engine)
+
+# load model
+model = joblib.load("../models/classifier.pkl")
+
+@app.route('/')
+@app.route('/index')
+def index():
+    '''Index webpage displays visuals and receives user input text for model'''
+    graphs = get_chart_object(df)
     # encode plotly graphs in JSON
     ids = ["graph-{}".format(i) for i, _ in enumerate(graphs)]
     graphJSON = json.dumps(graphs, cls=plotly.utils.PlotlyJSONEncoder)
@@ -114,9 +166,9 @@ def index():
     return render_template('master.html', ids=ids, graphJSON=graphJSON)
 
 
-# web page that handles user query and displays model results
 @app.route('/go')
 def go():
+    '''Web page that handles user query and displays model results'''
     # save user input in query
     query = request.args.get('query', '') 
 
